84984997bf46b137f89bf7f1bc7ec1b9c22e7ac8
Hyemin Kim <78579776+aemaaeng@users.noreply.github.com>
2022-08-26 23:18:10 +0900

    사분면

---
+++ 9610.py
@@ -0,0 +1,37 @@
+# 사분면
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+axis = 0
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+
+    # AXIS: x는 양수 or 0, y는 0
+    if y == 0 or x == 0:
+        axis += 1
+    
+    # Q1: x도 양수, y도 양수
+    elif x > 0 and y > 0:
+        q1 += 1
+    
+    # Q2: x는 음수, y는 양수
+    elif x < 0 and y > 0:
+        q2 += 1
+
+    # Q3: x도 음수, y도 음수
+    elif x < 0 and y < 0:
+        q3 += 1
+    
+    # Q4: x는 양수, y는 음수
+    elif x > 0 and y < 0:
+        q4 += 1
+    
+
+print("Q1: %d" %(q1))
+print("Q2: %d" %(q2))
+print("Q3: %d" %(q3))
+print("Q4: %d" %(q4))
+print("AXIS: %d" %(axis))
+
